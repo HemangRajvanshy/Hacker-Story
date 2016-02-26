5b46c2ab2c82b559857578c373e6344631d3af08
HemangRajvanshy <tobiuchiha1010@gmail.com>
2016-02-26 19:49:07 +0530

    Menu Scripts WIP.

---
+++ HackerStory Project/Assets/Scripts/Common/Main.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Main : MonoBehaviour {
+
+    public static Main Instance;
+
+
+    #region UnityMethods
+    void Awake()
+    {
+        Instance = this;
+    }
+    #endregion
+
+    #region publicMethods
+    public void LoadGameScene()
+    {
+        Debug.Log("TODO Load Game Scene");
+    }
+
+    public void LoadMenuScene()
+    {
+        Debug.Log("TODO: Load Menu Scene");
+    }
+
+    #endregion
+
+    #region privateMethods
+    private void ShowLoadingScreen()
+    {
+        Debug.Log("TODO: Show LoadingScreen");
+    }
+    #endregion
+}
---
+++ HackerStory Project/Assets/Scripts/Menu/Menu_UI.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu_UI : MonoBehaviour {
+
+    public GameObject PlayPanel;
+    public GameObject CreditsPanel;
+    
+    public void ShowCredits()
+    {
+        PlayPanel.SetActive(false);
+        CreditsPanel.SetActive(true);
+    }
+
+    public void CloseCredits()
+    {
+        PlayPanel.SetActive(true);
+        CreditsPanel.SetActive(false);
+    }
+}
