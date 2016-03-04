ff36d755aa782f0e2ab4ee021523fa9b2109f0df
HemangRajvanshy <tobiuchiha1010@gmail.com>
2016-03-04 18:36:52 +0530

    Desktop applications setup.

--- HackerStory Project/Assets/Scripts/Game/Desktop.cs
+++ HackerStory Project/Assets/Scripts/Game/Desktop.cs
@@ -3,13 +3,63 @@ using System.Collections;
 
 public class Desktop : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public GameObject HackerGuide;
+    public GameObject Browser;
+    public GameObject Computer;
+
+    private DesktopState state;
+    private enum DesktopState
+    {
+        Desktop,
+        HackerGuide,
+        Computer, 
+        Browser
+    };
+
+    void Start()
+    {
+        ShowDesktop();
+    }
+
+    public void ShowHackerGuide()
+    {
+        state = DesktopState.HackerGuide;
+        OpenApplication(HackerGuide);
+    }
+
+    public void ShowBrowser()
+    {
+        state = DesktopState.Browser;
+        OpenApplication(Browser);
+    }
+
+    public void ShowComputer()
+    {
+        state = DesktopState.Computer;
+        OpenApplication(Computer);
+    }
+
+    public void ShowDesktop()
+    {
+        state = DesktopState.Desktop;
+        HackerGuide.SetActive(false);
+        Browser.SetActive(false);
+        Computer.SetActive(false);
+    }
+
+    private void OpenApplication(GameObject App)
+    {
+        App.SetActive(true);
+    }
+
+    public void MinimizeApplication(GameObject App)
+    {
+        App.SetActive(false); //TODO
+        Debug.Log("called");
+    }
+
+    public void CloseApplication(GameObject App)
+    {
+        App.SetActive(false); //TODO
+    }
 }
