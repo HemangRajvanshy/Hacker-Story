a461ba524d451b27c3acff1c13e1d7afed19258b
HemangRajvanshy <tobiuchiha1010@gmail.com>
2016-03-05 19:15:12 +0530

    Game-project settings setup

--- HackerStory Project/Assets/Scripts/Game/Applications/HackerGuide.cs
+++ HackerStory Project/Assets/Scripts/Game/Applications/HackerGuide.cs
@@ -3,21 +3,52 @@ using UnityEngine.UI;
 using System.Collections.Generic;
 using System.Collections;
 
-public class HackerGuide : MonoBehaviour { //TODO
+public class HackerGuide : MonoBehaviour { 
 
     public List<Sprite> Pages;
+    public Button LastPageButton;
+    public Button NextPageButton;
+    public Image ContentImage;
 
     private int PageIndex;
 
 
+    void Start()
+    {
+        PageIndex = 0;
+        OpenPage(PageIndex);
+    }
+
     public void NextPage()
     {
+        if(PageIndex != Pages.Count-1)
+        {
+            PageIndex++;
+            OpenPage(PageIndex);
+        }
+    }
 
+    public void LastPage()
+    {
+        if (PageIndex != 0)
+        {
+            PageIndex--;
+            OpenPage(PageIndex);
+        }
     }
 
     public void OpenPage(int page)
     {
+        LastPageButton.interactable = true;
+        NextPageButton.interactable = true;
+        PageIndex = page;
 
+        if (page == 0)
+            LastPageButton.interactable = false;
+        if(page == Pages.Count-1)
+            NextPageButton.interactable = false;
+
+        ContentImage.sprite = Pages[page];
     }
      
 }
